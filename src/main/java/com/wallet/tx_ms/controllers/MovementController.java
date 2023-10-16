2b3f2c1b13d0fd2ccf00208d7c15f34b8eda380f
FelipeH22 <juan22herrera01@gmail.com>
2023-10-15 19:13:44 -0500

    Movements endpoint added

--- src/main/java/com/wallet/tx_ms/controllers/MovementController.java
+++ src/main/java/com/wallet/tx_ms/controllers/MovementController.java
@@ -3,17 +3,16 @@ package com.wallet.tx_ms.controllers;
 
 import com.wallet.tx_ms.dtos.MovementRecordDto;
 import com.wallet.tx_ms.models.MovementModel;
+import com.wallet.tx_ms.models.UserModel;
 import com.wallet.tx_ms.repositories.MovementRepository;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class MovementController {
@@ -31,5 +30,11 @@ public class MovementController {
     @GetMapping("/movements")
     public ResponseEntity<List<MovementModel>> getMovements(){
         return ResponseEntity.status(HttpStatus.OK).body(movementRepository.findAll());
+    }
+
+    @GetMapping("/movements/{id_user}")
+    public ResponseEntity<Object> getUser(@PathVariable(value="id_user") int id_user){
+        Optional<List<MovementModel>> movements = movementRepository.findByUserId(id_user);
+        return movements.<ResponseEntity<Object>>map(movementModel -> ResponseEntity.status(HttpStatus.OK).body(movementModel)).orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).body("User does not exist or does not have any movement yet."));
     }
 }
--- src/main/java/com/wallet/tx_ms/repositories/MovementRepository.java
+++ src/main/java/com/wallet/tx_ms/repositories/MovementRepository.java
@@ -1,9 +1,16 @@
 package com.wallet.tx_ms.repositories;
 
+import com.wallet.tx_ms.models.BalanceModel;
 import com.wallet.tx_ms.models.MovementModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface MovementRepository extends JpaRepository<MovementModel, UUID> {
+    @Query(value = "SELECT * FROM MOVEMENT WHERE id_user = :id_user ORDER BY datetime DESC", nativeQuery = true)
+    Optional<List<MovementModel>> findByUserId(@Param("id_user") Integer id_user);
 }
